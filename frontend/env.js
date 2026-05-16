494c4a2b87229a066c30badc38f754c189ff117e
Davit <111980053+D4V1TT@users.noreply.github.com>
2026-05-16 12:54:50 +0400

    update env.js

--- frontend/env.js
+++ frontend/env.js
@@ -10,3 +10,10 @@ window.HARAMBALL_CONFIG = {
   SUPABASE_URL:      'https://wolleqnvaonerzsomzvd.supabase.co',
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvbGxlcW52YW9uZXJ6c29tenZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzNTE1NTUsImV4cCI6MjA5MzkyNzU1NX0.eIG9qK4SdTsO3V5KVazvSCDZGKJ-9dN1w9ql5akNQ6M'
 };
+
+
+const STAGING = {
+  SUPABASE_URL:      'https://psoqcvpxnvbmffbzbzbv.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzb3FjdnB4bnZibWZmYnpiemJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4OTU0MTgsImV4cCI6MjA5NDQ3MTQxOH0.fTBAxCx8hJ2phZsp0ZwPnWmzEoN1xFknn5kX4zAxPn4',
+  ENV: 'staging'
+};
